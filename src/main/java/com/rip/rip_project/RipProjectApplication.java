4424b9fbdc0b09f4754a6f2ac5837762351c7c15
HolyDoly123 <vladimirudakovhd@gmail.com>
2021-10-15 10:53:07 +0300

    Initial commit

---
+++ src/main/java/com/rip/rip_project/RipProjectApplication.java
@@ -0,0 +1,13 @@
+package com.rip.rip_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RipProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RipProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rip/rip_project/RipProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rip.rip_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RipProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
