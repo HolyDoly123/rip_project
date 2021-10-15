140ea7fd3d2295be00c8c980e457bea98cc032bc
HolyDoly123 <vladimirudakovhd@gmail.com>
2021-10-15 20:50:43 +0300

    Initial commit

--- src/main/java/com/rip/rip_project/controllers/MainController.java
+++ src/main/java/com/rip/rip_project/controllers/MainController.java
@@ -3,7 +3,7 @@ package com.rip.rip_project.controllers;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "${heroku.address}", method = {RequestMethod.GET})
+//@RequestMapping(value = "ОБЩИЙ МАРШРУТ", method = {RequestMethod.GET})
 public class MainController {
     @GetMapping("/")
     public String index() {
