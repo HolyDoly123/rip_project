60acfc696a7cff0a3cd015d5a9505eaf16235a50
Galansev Egor <galancc@mail.ru>
2021-10-19 23:28:19 +0300

    Main view, соурс файлы для html и css(png svg ttf)

--- src/main/java/com/rip/rip_project/RipProjectApplication.java
+++ src/main/java/com/rip/rip_project/RipProjectApplication.java
@@ -2,9 +2,12 @@ package com.rip.rip_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @SpringBootApplication
 public class RipProjectApplication {
