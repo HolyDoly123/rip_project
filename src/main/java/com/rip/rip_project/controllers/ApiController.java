7c81b6261dbfe07e8a964dab022b5e0091b3ce40
Dmitry Malkov <dvm101@yandex.ru>
2021-10-19 21:59:53 +0300

    Доделал API

--- src/main/java/com/rip/rip_project/controllers/ApiController.java
+++ src/main/java/com/rip/rip_project/controllers/ApiController.java
@@ -2,12 +2,14 @@ package com.rip.rip_project.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class ApiController {
     @GetMapping("/Api")
     public String getApi() {
-        return "Api here";
-    }
+        RestTemplate restTemplate = new RestTemplate();
+        Fact fact = restTemplate.getForObject("https://asli-fun-fact-api.herokuapp.com", Fact.class);
+        return fact.toString();
+        }
 }
-
